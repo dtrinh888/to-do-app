01e2b2473e8fbafc86b53a3ca5a018565de4b244
dtrinh888 <dtrinh888@gmail.com>
2016-02-03 22:39:00 -0800

    able to remove tasks

--- app/js/app.js
+++ app/js/app.js
@@ -2,18 +2,25 @@ angular.module('toDoApp', [])
 	.controller('toDoCtrl', function($scope){
 		//set $scope variables
 		$scope.tasks = [];
+		$scope.deletedTasks = [];
 		$scope.submitTask = function(){
 			$scope.tasks.unshift($scope.enteredTask); 
 			$scope.enteredTask = '';
 		};
+		$scope.removeTask = function(task) {
+			var i = $scope.tasks.indexOf(task);
+			$scope.tasks.splice(i, 1);
+		};
 	})
-	.factory('toDoFactory', function(){
+	.factory('toDoFactory', ['$http', function($http){
 		//function to submit task
+		return function(newTask) {
 
+		};
 		//function to complete task
 		//function to remove task
 		//function to reset task
-	})
+	}])
 	.directive('toDo', function(){
 		return {
 			templateUrl: 'to-do.html',
