0baba6619d27bc43d6242ac256b3aeff5448d746
dtrinh888 <dtrinh888@gmail.com>
2016-02-04 22:15:17 -0800

    placed submitTask() and removeTask() into factory

--- app/js/app.js
+++ app/js/app.js
@@ -1,25 +1,30 @@
 angular.module('toDoApp', [])
-	.controller('toDoCtrl', function($scope){
+	.controller('toDoCtrl', function($scope, toDoFactory){
 		//set $scope variables
-		$scope.tasks = [];
-		$scope.deletedTasks = [];
+		$scope.tasks = toDoFactory.tasks;
 		$scope.submitTask = function(){
-			$scope.tasks.unshift($scope.enteredTask); 
+			toDoFactory.submitTask($scope.enteredTask); 
 			$scope.enteredTask = '';
 		};
-		$scope.removeTask = function(task) {
-			var i = $scope.tasks.indexOf(task);
-			$scope.tasks.splice(i, 1);
-		};
+		$scope.removeTask = toDoFactory.removeTask;
 	})
 	.factory('toDoFactory', ['$http', function($http){
 		//function to submit task
-		return function(newTask) {
-
+		var toDo = {
+			tasks: [],
+			enteredTask: '',
+			submitTask: function(task){
+				toDo.tasks.unshift(task);
+			},
+			removeTask: function(task) {
+				var i = toDo.tasks.indexOf(task);
+				toDo.tasks.splice(i, 1);
+			}
 		};
 		//function to complete task
 		//function to remove task
 		//function to reset task
+		return toDo;
 	}])
 	.directive('toDo', function(){
 		return {
